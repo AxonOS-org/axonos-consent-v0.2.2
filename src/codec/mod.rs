c60ea86b5a4f5b1b9a4396a8a46b57f3a71d36a6
Denis Yermakou <denissmartrich@gmail.com>
2026-04-12 07:51:26 +0800

    Delete src directory

--- src/codec/mod.rs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2026 Denis Yermakou / AxonOS
-// SPDX-License-Identifier: Apache-2.0 OR MIT
-//
-// This file is part of the AxonOS Consent Engine.
-// See LICENSE-APACHE or LICENSE-MIT for details.
-
-pub mod cbor;
-#[cfg(feature = "json")]
-pub mod json;
